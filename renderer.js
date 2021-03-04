4f8cb750525fa8876a1db0a87b1b662b06552b76
Arvid A <arvid.anderson@outlook.com>
2021-03-04 21:14:47 +0100

    init

--- renderer.js
+++ renderer.js
@@ -1,27 +1,27 @@
+//Importing neccesary modules for the database
 const Store = require('electron-store');
 const store = new Store();
 
 console.log(store.get());
 
-for (const property in store.get()) {
-    console.log(`${property}`);
-  }
-
-//Catch add crypto
-ul.className = 'no-collection-border collection';
-const li = document.createElement('li');
-li.className = 'custom-li collection-item';
-li.innerHTML = `
-<div class='row crypto-in-watchlist'>
-    <div class="col s6">
-        <h4 class='crypto-name'>${store.get('eth.name')}</h4><h5 class="crypto-price">${store.get('eth.price')}</h5>
-    </div>
-    <div class="col s6">
-        <img class="right" src="assets/cryptoicons/white/${store.get('eth.img')}.png" alt="">
-    </div> 
-</div>`
-ul.appendChild(li);
-
+// Rendering on launch - Also looping though the database to gather the information to render
+database_object = store.get();
+for (const i in database_object) {
+    ul.className = 'no-collection-border collection';
+    const li = document.createElement('li');
+    li.className = 'custom-li collection-item';
+    li.innerHTML = `
+    <div class='row crypto-in-watchlist'>
+        <div class="col s6">
+            <h4 class='crypto-name'>${i}</h4><h5 class="crypto-price">${store.get('eth.price')}</h5>
+        </div>
+        <div class="col s6">
+            <img class="right" src="assets/cryptoicons/white/${store.get('eth.img')}.png" alt="">
+        </div> 
+    </div>`
+    console.log(`${i}`);
+    ul.appendChild(li);
+}
 
 //Add to database
 function appendtoDatabase(crypto_name, crypto, result) {
