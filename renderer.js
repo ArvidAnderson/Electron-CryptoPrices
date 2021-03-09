a4d5952fd16446f4212b32b1c8ea919bbc8f178f
Arvid A <arvid.anderson@outlook.com>
2021-03-09 13:25:11 +0100

    Updated! Sortable ul
    Did break the updater sadly

--- renderer.js
+++ renderer.js
@@ -1,5 +1,6 @@
 //Importing neccesary modules for the database
 const Store = require('electron-store');
+const { watch } = require('original-fs');
 const store = new Store();
 
 // Rendering on launch - Also looping though the database to gather the information to render, function located below
@@ -9,23 +10,28 @@ renderWatchlist();
 function renderWatchlist() {
     database_object = store.get();
     for (const i in database_object) {
-        const name = store.get(`${i}.name`)
-        ul.className = 'no-collection-border collection';
-        const li = document.createElement('li');
-        li.className = 'custom-li collection-item';
-        callAPI('USD', i).then(result => {
-        li.innerHTML = `
-        <div class='row crypto-in-watchlist'>
-            <div class="col s6">
-                <h4>${name}</h4><h5>$${result}</h5>
-            </div>
-            <div class="col s6">
-                <img class="right" src="assets/cryptoicons/white/${i}.png" alt="">
-            </div> 
-        </div>`
-        })
-        ul.appendChild(li);
-    };
+        if (i == 'watchlist_order') {
+            //Do nothing
+        } else {
+            const name = store.get(`${i}.name`)
+            ul.className = 'no-collection-border collection';
+            ul.id = 'items';
+            const li = document.createElement('li');
+            li.className = 'custom-li collection-item';
+            li.setAttribute('data-id', `${name}`)
+            callAPI('USD', i).then(result => {
+            li.innerHTML = `
+            <div class='row crypto-in-watchlist'>
+                <div class="col s6">
+                    <h4>${name}</h4><h5>$${result}</h5>
+                </div>
+                <div class="col s6">
+                    <img class="right" src="assets/cryptoicons/white/${i}.png" alt="">
+                </div> 
+            </div>`
+            })
+            ul.appendChild(li);
+    };  }
 }
 
 //Catch add crypto - On add
@@ -39,6 +45,7 @@ ipcRenderer.on('crypto:add', function(e, crypto, crypto_name){
         ul.className = 'no-collection-border collection';
         const li = document.createElement('li');
         li.className = 'custom-li collection-item';
+        li.setAttribute('data-id', `${crypto_name}`)
         //Adding the price using the API from crypto_api_js
         callAPI('USD', crypto).then(result => {
             li.innerHTML = (`
@@ -73,4 +80,32 @@ ipcRenderer.on('watchlist:clear', function(){
 ipcRenderer.on('watchlist:reload', function(){
     ul.innerHTML = '';
     renderWatchlist();
+});
+
+const Sortable = require('sortablejs');
+var el = document.getElementById('items');
+var sortable = Sortable.create(el, {
+    animation: 600,
+    dataIdAttr: 'data-id',
+    group: 'watchlist_order',
+    store: {
+            /**
+             * Get the order of elements. Called once during initialization.
+             * @param   {Sortable}  sortable
+             * @returns {Array}
+             */
+            get: function (sortable) {
+                var order = store.get(sortable.options.group.name);
+                return order ? order.split('|') : [];
+            },
+
+            /**
+             * Save the order of elements. Called onEnd (when the item is dropped).
+             * @param {Sortable} sortable
+             */
+            set: function (sortable) {
+                var order = sortable.toArray();
+                store.set(sortable.options.group.name, order.join('|'));
+            }
+    }
 });
